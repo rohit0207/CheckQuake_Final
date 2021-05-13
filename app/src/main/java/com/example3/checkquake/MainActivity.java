58b7e5c1e0ba9041ee08e420b0eab206ebb1a55a
rohit0207 <68811221+rohit0207@users.noreply.github.com>
2021-05-13 23:21:08 +0530

    Instructions page is added with offline video

--- app/src/main/java/com/example3/checkquake/MainActivity.java
+++ app/src/main/java/com/example3/checkquake/MainActivity.java
@@ -9,25 +9,28 @@ import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
     ImageButton search;
+    ImageButton instButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        search =(ImageButton)findViewById(R.id.bus1);
-        search.setOnClickListener(new View.OnClickListener() {
+        search = (ImageButton)findViewById(R.id.bus1);
+        instButton = (ImageButton)findViewById(R.id.instButton);
+
+        search.setOnClickListener(new View.OnClickListener()   {
             public void onClick(View v)  {
-
-                Intent i = new Intent(MainActivity.this,
-                        MapsActivity.class);
-                startActivity(i);
-
-
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(intent);
             }
         });
 
-
-
-
+        instButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, instructions.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example3/checkquake/instructions.java
+++ app/src/main/java/com/example3/checkquake/instructions.java
@@ -7,12 +7,14 @@ import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class instructions extends AppCompatActivity {
+    VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.instructions);
-        VideoView videoView = findViewById(R.id.videoView);
+
+        videoView = findViewById(R.id.videoView);
         videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.disaster);
         MediaController mediaController = new MediaController(this);
         mediaController.setAnchorView(videoView);
