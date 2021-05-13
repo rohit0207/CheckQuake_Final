8f82e96f17eaa9607b3a223e94c2d64c914edd54
rohit0207 <68811221+rohit0207@users.noreply.github.com>
2021-05-13 18:37:28 +0530

    Instructions page is added with offline video

--- app/src/main/java/com/example3/checkquake/MainActivity.java
+++ app/src/main/java/com/example3/checkquake/MainActivity.java
@@ -2,12 +2,24 @@ package com.example3.checkquake;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageButton instButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        instButton = (ImageButton) findViewById(R.id.instButton);
+        instButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
