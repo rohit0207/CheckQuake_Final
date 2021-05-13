caab84afe1a643c41799291fac7d8592b6c26fb9
abhishek1052 <68838873+abhishek1052@users.noreply.github.com>
2021-05-13 20:26:11 +0530

    setting up the google map api and earthquake api

--- app/src/main/java/com/example3/checkquake/MainActivity.java
+++ app/src/main/java/com/example3/checkquake/MainActivity.java
@@ -2,13 +2,32 @@ package com.example3.checkquake;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageButton search;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        search =(ImageButton)findViewById(R.id.bus1);
+        search.setOnClickListener(new View.OnClickListener()   {
+            public void onClick(View v)  {
+
+                Intent intent = new Intent(MainActivity.this,
+                        MapsActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/example3/checkquake/UTIL/Constants.java
@@ -0,0 +1,9 @@
+package com.example3.checkquake.UTIL;
+
+public class Constants {
+
+public static final String url="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+public static final int LIMIT=30;
+
+
+}
