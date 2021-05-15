50181c37fd91af62417431a62ae810a09b7f1f9c
WahAkshat <jeet11chatterjee@gmail.com>
2021-05-15 15:01:16 +0530

    Added location in SOS

--- app/src/main/java/com/example3/checkquake/SOS.java
+++ app/src/main/java/com/example3/checkquake/SOS.java
@@ -30,7 +30,7 @@ public class SOS extends AppCompatActivity {
             @Override
             public void onClick(View arg0) {
                 String to = "jeet11chatterjee@gmail.com,rohitmantri0207@gmail.com";
-                String message = "Hi the location of the person is:"+Mylocation.latitude_k+Mylocation.longitude_k;
+                String message = "Hi the location of the person is:\n"+Mylocation.latitude_k+" N\n"+Mylocation.longitude_k+" E";
 
                 Intent email = new Intent(Intent.ACTION_SEND);
                 email.putExtra(Intent.EXTRA_EMAIL, new String[]{to});
