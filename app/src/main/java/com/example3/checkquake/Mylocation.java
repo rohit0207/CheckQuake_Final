f6a823f7152991dd93d2003dfae2e087d49e8200
abhishek1052 <68838873+abhishek1052@users.noreply.github.com>
2021-05-15 13:48:20 +0530

    location class

---
+++ app/src/main/java/com/example3/checkquake/Mylocation.java
@@ -0,0 +1,12 @@
+package com.example3.checkquake;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class Mylocation {
+    public Double latitude_bhiwandi=19.2813;
+    public Double longitude_bhiwandi=73.0483;
+    public Double latitude_jodhpur=26.2389;
+    public Double longitude_jodhpur=73.0243;
+    public Double latitude_kolkata=22.5726;
+    public Double longitude_kolkata=19.2813;
+}
